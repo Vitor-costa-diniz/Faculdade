01b42508fcbc83a91cbb33659268ec0a5d59e312
Vitor-costa-diniz <78705027+Vitor-costa-diniz@users.noreply.github.com>
2023-03-02 14:00:38 -0300

    add element to an vector

---
+++ Estrtura_de_Dados/Teste/Aula03.dart
@@ -0,0 +1,9 @@
+import '../Vetor/vetor.dart';
+
+void main() {
+  Vetor vetor = Vetor(2);
+
+  vetor.adicionar('elemento 1');
+  vetor.adicionar('elemento 2');
+  vetor.adicionar('elemento 3');
+}
--- Estrtura_de_Dados/Vetor/vetor.dart
+++ Estrtura_de_Dados/Vetor/vetor.dart
@@ -1,7 +1,40 @@
-class Vetor {
-  dynamic _elementos;
+import 'dart:core';
 
-  Vetor(int capadcidade) {
-    this._elementos = capadcidade;
+class Vetor {
+  List<dynamic> _elementos = [];
+  late int _tamanaho;
+
+  Vetor(int capacidade) {
+    this._elementos = List<dynamic>.filled(capacidade, null);
+    this._tamanaho = 0;
+  }
+/*
+  adicionar(String elemento) {
+    for (int i = 0; i < this._elementos.length; i++) {
+      if (this._elementos[i] == null) {
+        this._elementos[i] = elemento;
+        break;
+      }
+    }
+  }
+  */
+
+  /*adicionar(String elemento) {
+    if (this._tamanaho < this._elementos.length) {
+      this._elementos[this._tamanaho] = elemento;
+      this._tamanaho++;
+    } else {
+      throw Exception(
+          'Vetor já está cheio não é possível adicionar mais elemtnos');
+    }
+  }*/
+
+  bool adicionar(String elemento) {
+    if (this._tamanaho < this._elementos.length) {
+      this._elementos[this._tamanaho] = elemento;
+      this._tamanaho++;
+      return true;
+    }
+    return false;
   }
 }
